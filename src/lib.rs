7e971ff0a096d30b39ff7abb5a81e2c101ee022e
shadow3aaa <shadow3aaa@qq.com>
2023-11-27 22:19:53 +0800

    完成注入write@LIBC的准备

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![allow(clippy::missing_safety_doc)]
+use std::mem;
+
+use ctor::ctor;
+use dobby_api::{hook, Address};
+use libc::{c_int, c_void, size_t, ssize_t};
+
+static mut LIBC_WRITE: Address = libc::write as Address;
+
+#[ctor]
+unsafe fn patch_main() {
+    let _ = hook(
+        libc::write as Address,
+        write as Address,
+        Some(&mut LIBC_WRITE),
+    );
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[inline(always)]
+pub unsafe extern "C" fn libc_write_ori(fd: c_int, buf: *const c_void, count: size_t) -> ssize_t {
+    let ori_write: extern "C" fn(c_int, *const c_void, size_t) -> ssize_t =
+        mem::transmute(LIBC_WRITE);
+    ori_write(fd, buf, count)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub unsafe extern "C" fn write(fd: c_int, buf: *const c_void, count: size_t) -> ssize_t {
+    println!("a");
+    libc_write_ori(fd, buf, count)
 }
